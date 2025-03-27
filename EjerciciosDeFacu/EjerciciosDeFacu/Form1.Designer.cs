51b9baa6c87fe00ff0807ba8ec9e33ea804bc172
MatiasGalego <matiasgalego23@gmail.com>
2025-03-27 00:06:43 -0300

    Inicio de ejericio

--- EjerciciosDeFacu/EjerciciosDeFacu/Form1.Designer.cs
+++ EjerciciosDeFacu/EjerciciosDeFacu/Form1.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,18 +23,86 @@
         #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        /// Método requerido para admitir el Diseñador.  
+        /// No se puede modificar el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(57, 32);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(57, 16);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.AccessibleDescription = "txtUsuario1";
+            this.txtUsuario.AccessibleName = "txtUsuario1";
+            this.txtUsuario.Location = new System.Drawing.Point(171, 32);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(228, 22);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(57, 75);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(79, 16);
+            this.lblContraseña.TabIndex = 2;
+            this.lblContraseña.Text = "Contraseña:";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(171, 75);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.Size = new System.Drawing.Size(228, 22);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(171, 117);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(114, 32);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(457, 213);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnIngresar;
     }
 }
-
--- EjerciciosDeFacu/EjerciciosDeFacu/Form1.cs
+++ EjerciciosDeFacu/EjerciciosDeFacu/Form1.cs
@@ -12,9 +12,20 @@ namespace EjerciciosDeFacu
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private void btnIngresar_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            string usuario = txtUsuario.Text;  // Aquí cambias el nombre
+            string contraseña = txtContraseña.Text;  // Aquí también
+
+            // Validación simple
+            if (usuario == "admin" && contraseña == "1234")
+            {
+                MessageBox.Show($"¡Bienvenido {usuario}!", "Acceso Concedido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
